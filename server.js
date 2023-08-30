3f575e7fe837bc8742df8d063a6ed0729e488352
nhutmai <nhutmai002@gmail.com>
2023-08-30 11:39:15 +0700

    addjwt

--- server.js
+++ server.js
@@ -3,13 +3,31 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const productmodel = require("./productModel");
 const accountmodel = require("./accountmodel");
+var jwt = require("jsonwebtoken");
 
 const app = express();
 const port = 4000;
 app.use(cors());
+
 //midleway bodyparser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//check login fuction
+const checklogin = (req, res, next) => {
+  var decoded = jwt.verify(req.body.token, "nhut");
+  accountmodel
+    .findById(decoded.userId)
+    .then((data) => {
+      if (data) {
+        res.data = data;
+        next();
+      } else {
+        res.json("vui lòng đăng nhập");
+      }
+    })
+    .catch((err) => console.log("lỗi xác thực token"));
+};
 
 //return product
 app.get("/product", (req, res) => {
@@ -20,20 +38,30 @@ app.get("/product", (req, res) => {
       console.log(err);
     });
 });
+//user account
+app.post("/account", checklogin, (req, res) => {
+  res.json(res.data);
+});
 
 //checking login
-app.post("/account", (req, res) => {
+app.post("/login", (req, res) => {
   const { name, pass } = req.body;
+  console.log(name);
   accountmodel
     .findOne({ name: name, pass: pass })
     .then((data) => {
-      res.json("kq lad " + data);
+      if (data) {
+        const token = jwt.sign({ userId: data._id }, "nhut", {
+          expiresIn: "1h",
+        });
+        return res.json({ token });
+      } else {
+        res.status(401).json({ message: "Tài khoản không tồn tại" });
+      }
     })
     .catch((err) => {
       res.status(500).json(err);
     });
 });
 
-app.listen(port, () => {
-  console.log(`App is listening in ${port}`);
-});
+app.listen(port, () => {});
